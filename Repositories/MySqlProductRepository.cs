fe33b59ff096e6e14f8b41945b3df48e2ae20582
Una <yuyucating@gmail.com>
2025-07-01 21:21:02 +0800

    Add GetAllProducts & GetProductByID

--- Repositories/MySqlProductRepository.cs
+++ Repositories/MySqlProductRepository.cs
@@ -9,18 +9,19 @@ public class MySqlProductRepository : IProductRepository
     private readonly string _connectionString;
     public MySqlProductRepository(string connectionString)
     {
-        _connectionString = connectionString;
+        _connectionString = connectionString; // 接收外界的資料
         InitializeDatabase();
     }
 
     private void InitializeDatabase()
     {
         // using: 以防忘記關掉, 而占用資源
-        using (var connection = new MySqlConnection(_connectionString))
+        using (var connection = new MySqlConnection(_connectionString)) // MySqlConnection 接收連接需要的物件
         {
             try
             {
-                connection.Open(); //開啟連線
+                connection.Open(); //開啟連結 (進行連線)
+                //建立 SQL 語句
                 string createTableSql = @"
                     create table if not exists product(
                         id INT PRIMARY KEY AUTO_INCREMENT,
@@ -32,7 +33,7 @@ public class MySqlProductRepository : IProductRepository
      
                 "; // @可以換行的字串 (???) , 可以跟 $"" 做比較???
                 // cmd 命令: 把我們的字串(連線) 執行 connection
-                using MySqlCommand cmd = new MySqlCommand(createTableSql, connection);
+                using MySqlCommand cmd = new MySqlCommand(createTableSql, connection); // MySqlCommand 執行 SQL 語句(?)
                 {
                     cmd.ExecuteNonQuery(); //執行
                 }
@@ -47,11 +48,72 @@ public class MySqlProductRepository : IProductRepository
 
     public List<Product> GetAllProducts()
     {
-        throw new NotImplementedException();
+        List<Product> products = new List<Product>();
+        using (var connection = new MySqlConnection(_connectionString))
+        {
+            connection.Open();
+            string selectSql = "SELECT * FROM product"; // 所有 product 這個 table 裡的資料 (很多個 product)
+            using (MySqlCommand cmd = new MySqlCommand(selectSql, connection))
+            {
+                using (MySqlDataReader reader = cmd.ExecuteReader())  // 他會取得很多筆, 自己會迭代, 所以要搭配 while!!
+                {   
+                    // 一筆資料一筆資料讀取 (列)
+                    while (reader.Read())  // 因為不知道有幾筆資料, 所以用 while()
+                    {
+                        // 將獨到的資料丟掉 Product 裡面!! 再丟到 List 裡面收起來 [obj initializer]
+                        products.Add(new Product(reader.GetInt32("id"), 
+                            reader.GetString("name"), 
+                            reader.GetDecimal("price"), 
+                            reader.GetInt32("quantity"))
+                        {
+                            Status = (Product.ProductStatus)reader.GetInt32("status")
+                        });
+                        
+                        // 傳統寫法:
+                        // Product product = new Product(reader.GetInt32("id"),
+                        //     reader.GetString("name"),
+                        //     reader.GetDecimal("price"),
+                        //     reader.GetInt32("quantity"));
+                        // product.Status = (Product.ProductStatus)reader.GetInt32("status");
+                        // products.Add(product);
+                        
+                    }
+                }
+            }
+        }
+
+        return products;
     }
 
     public Product GetProductById(int id)
     {
-        throw new NotImplementedException();
+        Product product = null;
+        using (var connection = new MySqlConnection(_connectionString))
+        {
+            connection.Open();
+            string selectSql = "SELECT * FROM product WHERE id = @id";
+            // string selectSql = "SELECT * FROM product WHERE id = " + id;
+            using (MySqlCommand cmd = new MySqlCommand(selectSql, connection))
+            {
+                // 防止 SQL Injection
+                cmd.Parameters.AddWithValue("@id", id);
+                using (MySqlDataReader reader = cmd.ExecuteReader()) // 他會取得很多筆, 自己會迭代, 所以要搭配 while!!
+                {
+                    if (reader.Read())
+                    {
+                        // obj initializer
+                        product = new Product(reader.GetInt32("id"),
+                            reader.GetString("name"),
+                            reader.GetDecimal("price"),
+                            reader.GetInt32("quantity"))
+                        {
+                            Status = (Product.ProductStatus)reader.GetInt32("status")
+                        };
+                    }
+                }
+            }
+        }
+
+        return product;
     }
 }
